935b02ab89690f30758743553f05dc0d9b4f4861
igorSobayev <isobayev@gmail.com>
2019-03-10 08:28:46 +0100

    Empezando a trabajar con las rutas y contenido dinámico del sitio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ContactoComponent } from './contacto/contacto.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { InicioComponent } from './inicio/inicio.component';
+
+// const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  // configuro rutas para el inicio
+  { path: '', component: InicioComponent, pathMatch: 'full'},
+  { path: '**', redirectTo: '/', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+// import { RouterModule, Routes } from '@angular/router';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,12 +7,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { FooterComponent } from './footer/footer.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { InicioComponent } from './inicio/inicio.component';
+
+
+// const routes: Routes = [
+//   { path: 'contacto', component: ContactoComponent }
+// ];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    FooterComponent
+    FooterComponent,
+    EncabezadoComponent,
+    CuerpoComponent,
+    ContactoComponent,
+    NosotrosComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
